0a9b96a9e137650b70b1ad9b081dcecc3f232c06
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-04-09 15:56:46 +0300

    Add data generation

---
+++ src/main/java/top/offsetmonkey538/fluidlib/datagen/DataGeneration.java
@@ -0,0 +1,14 @@
+package top.offsetmonkey538.fluidlib.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGeneration implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        // TODO: Add data generators
+    }
+}
