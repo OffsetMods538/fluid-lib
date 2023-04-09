671831612b1642f947624dbc033c136b71bb161c
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-04-09 21:52:10 +0300

    Something related to pathfinding I'm not really sure what this is

---
+++ src/main/java/top/offsetmonkey538/fluidlib/mixin/block/SlabBlockMixin.java
@@ -0,0 +1,25 @@
+package top.offsetmonkey538.fluidlib.mixin.block;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import top.offsetmonkey538.fluidlib.IFluid;
+
+@Mixin(SlabBlock.class)
+public abstract class SlabBlockMixin {
+
+    @ModifyExpressionValue(
+            method = "canPathfindThrough",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/fluid/FluidState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"
+            )
+    )
+    private boolean fluidlib$allowPathfindingThroughCustomFluid(boolean original, BlockState state, BlockView world, BlockPos pos) {
+        return original || world.getFluidState(pos).getFluid() instanceof IFluid;
+    }
+}
