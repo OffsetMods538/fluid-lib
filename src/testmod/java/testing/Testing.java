df0321b08d15fd43eafb0a6aebe4de42a4edf975
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-04-10 20:03:39 +0300

    [TESTMOD] Fix fluid rendering

--- src/testmod/java/testing/Testing.java
+++ src/testmod/java/testing/Testing.java
@@ -1,16 +1,33 @@
 package testing;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.util.Identifier;
 import testing.init.ModBlocks;
 import testing.init.ModFluids;
 import testing.init.ModItems;
 
-public class Testing implements ModInitializer {
+public class Testing implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
         ModFluids.register();
         ModBlocks.register();
         ModItems.register();
+    }
+
+    @Override
+    public void onInitializeClient() {
+        FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_TESTING_FLUID, ModFluids.FLOWING_TESTING_FLUID, new SimpleFluidRenderHandler(
+                new Identifier("minecraft:block/water_still"),
+                new Identifier("minecraft:block/water_flow"),
+                0x79cd3c
+        ));
+
+        BlockRenderLayerMap.INSTANCE.putFluids(RenderLayer.getTranslucent(), ModFluids.STILL_TESTING_FLUID, ModFluids.FLOWING_TESTING_FLUID);
     }
 }
