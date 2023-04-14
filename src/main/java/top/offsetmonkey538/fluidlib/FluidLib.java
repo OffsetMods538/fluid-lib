db9cd32084e053d4d1a6cc3572c7b53c8a5e1ed6
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-04-14 15:05:21 +0300

    Add logger for fluid-lib

--- src/main/java/top/offsetmonkey538/fluidlib/FluidLib.java
+++ src/main/java/top/offsetmonkey538/fluidlib/FluidLib.java
@@ -1,6 +1,8 @@
 package top.offsetmonkey538.fluidlib;
 
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class FluidLib {
     private FluidLib() {
@@ -8,6 +10,7 @@ public final class FluidLib {
     }
 
     public static final String MOD_ID = "fluid-lib";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static Identifier id(String path) {
         return new Identifier(MOD_ID, path);
