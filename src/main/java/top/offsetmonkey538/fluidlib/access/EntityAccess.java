e710e17bdb0c18e32d5289ae3e76152f69e6efce
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-07-10 19:39:02 +0300

    More methods for checking if ann entity has collided with a specific fluid

--- src/main/java/top/offsetmonkey538/fluidlib/access/EntityAccess.java
+++ src/main/java/top/offsetmonkey538/fluidlib/access/EntityAccess.java
@@ -1,7 +1,36 @@
 package top.offsetmonkey538.fluidlib.access;
 
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import top.offsetmonkey538.fluidlib.api.FluidBehaviourRegistry;
 import top.offsetmonkey538.fluidlib.api.IFluidBehaviour;
+import top.offsetmonkey538.fluidlib.impl.FluidBehaviourRegistryImpl;
 
+/**
+ * Use for checking if an entity has collided with a fluid.
+ */
 public interface EntityAccess {
     boolean hasCollidedWith(IFluidBehaviour behaviour);
+    default boolean hasCollidedWith(Fluid fluid) {
+        final boolean[] returnValue = {false};
+
+        ((FluidBehaviourRegistryImpl) FluidBehaviourRegistry.INSTANCE).forEach((fluid2, behaviour) -> {
+            if (fluid != fluid2) return;
+            if (!hasCollidedWith(behaviour)) return;
+            returnValue[0] = true;
+        });
+
+        return returnValue[0];
+    }
+    default boolean hasCollidedWith(FluidState fluidState) {
+        final boolean[] returnValue = {false};
+
+        ((FluidBehaviourRegistryImpl) FluidBehaviourRegistry.INSTANCE).forEach((fluid2, behaviour) -> {
+            if (fluidState.isIn(behaviour.getTagKey())) return;
+            if (!hasCollidedWith(behaviour)) return;
+            returnValue[0] = true;
+        });
+
+        return returnValue[0];
+    }
 }
